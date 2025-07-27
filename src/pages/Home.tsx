6f20ed486d66810b9dcc7fb1a2822ba11512b171
Ashuksu <ashuksu@gmail.com>
2025-07-27 11:29:21 +0200

    master - added Home page component with basic layout and styling

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <h1 className="text-4xl font-bold">"Forestman" Woodcraft</h1>
+            <p className="mt-4">Welcome to our workshop.</p>
+            {/*TODO: Create the home page content*/}
+        </div>
+    )
+}
+
+export default Home
