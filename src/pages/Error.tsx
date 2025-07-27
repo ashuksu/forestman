84e4954a1fd0f65cdfb009778b8fafa4601c2881
Ashuksu <ashuksu@gmail.com>
2025-07-27 12:00:08 +0200

    master - added Error component for 404 page handling

---
+++ src/pages/Error.tsx
@@ -0,0 +1,13 @@
+function Error() {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <div className="flex min-h-80 items-center justify-center flex-col text-center">
+                <h1 className="text-4xl font-bold mb-4">404</h1>
+                <p>No pages found</p>
+                {/*TODO: Create the Error page content*/}
+            </div>
+        </div>
+    )
+}
+
+export default Error
