3a618502f5852e63582b0416b8e95a59e7cf0d73
Ashuksu <ashuksu@gmail.com>
2025-07-27 11:29:52 +0200

    master - refactored App component to include Header and Footer, and set up routing with Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import tailwindCssLogo from './assets/tailwindcss.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Outlet} from 'react-router-dom'
+import Header from './components/layout/Header.tsx'
+import Footer from './components/layout/Footer.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+    // TODO: The logic for determining and setting the theme (light/dark) will be here
 
-  return (
-      <>
-          <div className="flex items-center justify-center gap-4>">
-              <a href="https://vite.dev" target="_blank">
-                  <img src={viteLogo} className="logo vite" alt="Vite logo"/>
-              </a>
-              <a href="https://react.dev" target="_blank">
-                  <img src={reactLogo} className="logo react" alt="React logo"/>
-              </a>
-              <a href="https://tailwindcss.com/" target="_blank">
-                  <img src={tailwindCssLogo} className="logo tailwindcss" alt="Tailwind CSS logo"/>
-              </a>
-          </div>
-          <h1 className="text-3xl font-bold">React + TypeScript + Vite + Tailwind CSS</h1>
-          <div className="card flex flex-col items-center justify-center space-y-6">
-              <button onClick={() => setCount((count) => count + 1)}>
-                  count is {count}
-              </button>
-              <p>
-                  Edit <code>src/App.tsx</code> and save to test HMR
-              </p>
-          </div>
-          <p className="read-the-docs">
-              Click on the Vite and React logos to learn more
-          </p>
-      </>
-  )
+    return (
+        <div className="flex min-h-screen flex-col">
+            <Header/>
+            <main className="flex-grow">
+                <Outlet/>
+            </main>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App
