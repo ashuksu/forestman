c07a5a8d282097b700f3ef7992e6e7d7be7ce3c3
Ashuksu <ashuksu@gmail.com>
2025-07-27 12:54:31 +0200

    master - added vite-tsconfig-paths for improved path aliasing support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
+// import packageJson from './package.json';
+// const repoName = packageJson.name;
+
 export default defineConfig({
+    // For GitHub Pages
+    // base: `/${repoName}/`,
+
     plugins: [
         react(),
         tailwindcss(),
+        tsconfigPaths(),
     ],
-})
+});
