f9d400e370f84b20f2662d1e84c08c77ec771d88
Ashuksu <ashuksu@gmail.com>
2025-07-27 12:54:45 +0200

    master - added About and Catalog pages with layout management

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import {Container} from "~/components/layout/Container";
+
+export default function About() {
+    return (
+        <Container className="py-8">
+            <div className="flex min-h-80 items-center justify-center flex-col text-center">
+                <h1 className="text-4xl font-bold mb-4">About Page</h1>
+                {/* TODO: Create the Error page content */}
+            </div>
+        </Container>
+    )
+}
---
+++ src/pages/Catalog.tsx
@@ -0,0 +1,12 @@
+import {Container} from "~/components/layout/Container";
+
+export default function Catalog() {
+    return (
+        <Container className="py-8">
+            <div className="flex min-h-80 items-center justify-center flex-col text-center">
+                <h1 className="text-4xl font-bold mb-4">Catalog Page</h1>
+                {/* TODO: Create the Catalog page content */}
+            </div>
+        </Container>
+    )
+}
