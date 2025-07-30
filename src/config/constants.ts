cc8767c128926fe0a47a551d4fa6e832589a7b4e
Ashuksu <ashuksu@gmail.com>
2025-07-30 19:28:04 +0200

    master - added project metadata constants including version, author, and company details

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,17 @@
 import packageJson from '../../package.json';
+
 export const PROJECT_NAME = packageJson.name ?? 'forestman';
-export const PROJECT_SHORT_NAME = 'Forestman';
-export const PROJECT_DISPLAY_NAME = '"Forestman" Woodcraft';
+export const PROJECT_VERSION = packageJson.version ?? '0.0.0';
+export const PROJECT_AUTHOR = packageJson.author ?? 'ashuksu';
+export const PROJECT_AUTHOR_URL = 'https://github.com/ashuksu';
+export const PROJECT_AUTHOR_EMAIL = 'ashuksu@gmail.com';
+
+export const COMPANY_NAME = 'Forestman';
+export const COMPANY_SHORT_NAME = 'forestman';
+export const COMPANY_EMAIL = 'email@example.com';
+export const COMPANY_PHONE = '+1234567890';
+
+export const WEBSITE_URL = 'https://yourwebsite.com/';
 // export const BASE_PATH = `/${PROJECT_NAME}`;
 export const BASE_PATH = '/';
 export const APP_PATH = '/';
