5e1d8be7a2054076e34f1b162f4967ff08f02130
Ashuksu <ashuksu@gmail.com>
2025-07-31 22:21:19 +0200

    master - added FeedbackForm component to About page and updated layout for better responsiveness

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,6 +2,7 @@ import {Container} from "~/components/layout/Container";
 import PageMeta from '~/components/PageMeta';
 import {useTranslation} from 'react-i18next';
 import Contacts from "~/components/Contacts";
+import FeedbackForm from "~/components/FeedbackForm";
 
 export default function About() {
     const {t} = useTranslation();
@@ -17,15 +18,18 @@ export default function About() {
                     {/* TODO: Create the About page content */}
                 </div>
             </Container>
-            <Container className="py-8">
-                <div className="flex gap-4">
-                    <div className="flex-1/2">
+            <Container className="py-15 lg:pb-25">
+                <div className="flex flex-col-reverse lg:flex-row justify-center gap-x-6 gap-y-20">
+                    <div className="flex-1 lg:flex-1/2 max-w-xl mx-auto lg:mx-0">
                         <h2 className="text-2xl font-bold mb-2">{t('contacts.heading')}</h2>
                         <p className="text-lg text-gray-700 mb-4">{t('contacts.text')}</p>
                         <Contacts/>
+                        <p className="text-lg text-gray-700 mt-6 whitespace-pre-line">{t('contacts.contactNote')}</p>
                     </div>
-                    <div className="flex-1/2">
-                        {/*TODO: add a feedback form*/}
+                    <div className="flex-1 lg:flex-1/2 max-w-xl mx-auto lg:mx-0">
+                        <h2 className="text-2xl font-bold mb-2">{t('form.heading')}</h2>
+                        <p className="text-lg text-gray-700 mb-4">{t('form.text')}</p>
+                        <FeedbackForm/>
                     </div>
                 </div>
             </Container>
