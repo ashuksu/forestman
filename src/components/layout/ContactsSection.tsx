9079244798be0609a3d70e589d5af1a3a02fb3e5
Ashuksu <ashuksu@gmail.com>
2025-08-03 16:33:17 +0200

    master - added ContactsSection component to integrate Contacts and FeedbackForm

---
+++ src/components/layout/ContactsSection.tsx
@@ -0,0 +1,28 @@
+import {useTranslation} from "react-i18next";
+import Contacts from "~/components/Contacts";
+import FeedbackForm from "~/components/FeedbackForm";
+import {Container} from "~/components/layout/Container";
+
+export default function ContactsSection() {
+    const {t} = useTranslation();
+
+    return (
+        <section id="contacts-section">
+            <Container className="py-15 lg:pb-25">
+                <div id="contacts" className="flex flex-col-reverse lg:flex-row justify-center gap-x-6 gap-y-20">
+                    <div className="flex-1 lg:flex-1/2 max-w-xl mx-auto lg:mx-0">
+                        <h2 className="text-2xl font-bold mb-2">{t('contacts.heading')}</h2>
+                        <p className="text-lg text-gray-700 mb-4">{t('contacts.text')}</p>
+                        <Contacts/>
+                        <p className="text-lg text-gray-700 mt-6 whitespace-pre-line">{t('contacts.contactNote')}</p>
+                    </div>
+                    <div className="flex-1 lg:flex-1/2 max-w-xl mx-auto lg:mx-0">
+                        <h2 className="text-2xl font-bold mb-2">{t('form.heading')}</h2>
+                        <p className="text-lg text-gray-700 mb-4">{t('form.text')}</p>
+                        <FeedbackForm/>
+                    </div>
+                </div>
+            </Container>
+        </section>
+    );
+}
