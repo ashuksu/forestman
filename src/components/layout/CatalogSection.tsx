7463fce193fc45380dfa91bc75f5ef8af79c6d63
Ashuksu <ashuksu@gmail.com>
2025-08-12 15:12:00 +0200

    master - added catalog section with translations and layout

---
+++ src/components/layout/CatalogSection.tsx
@@ -0,0 +1,36 @@
+import {useTranslation} from 'react-i18next';
+import Title from '~/components/ui/Title';
+import Button from '~/components/ui/Button';
+import {BASE_PATH} from '~/config/constants';
+import Image from '~/assets/images/pictute2.jpeg';
+
+export default function AboutSection() {
+    const {t} = useTranslation();
+
+    return (
+        <section id="catalog-section" className="pt-12 pb-16">
+            <div
+                className="w-full max-w-[var(--2xl)] mx-auto px-4 md:px-0 flex flex-col md:flex-row items-center md:items-stretch gap-6">
+                <div className="w-full md:w-1/2 flex flex-col items-center md:items-end text-center md:text-right p-6">
+                    <Title className="text-center">{t('catalogSection.heading')}</Title>
+
+                    <p className="text-lg text-gray-700 mb-6">
+                        {t('catalogSection.text')}
+                    </p>
+
+                    <Button href={`${BASE_PATH}catalog`}>
+                        {t('catalogSection.buttonText')}
+                    </Button>
+                </div>
+                <div className="w-full md:w-1/2">
+                    <img
+                        src={Image}
+                        alt={t('catalogSection.imageAlt')}
+                        loading="lazy"
+                        className="w-full h-auto md:h-full object-cover rounded-xl md:rounded-r-none 2xl:rounded-xl shadow-md"
+                    />
+                </div>
+            </div>
+        </section>
+    );
+}
