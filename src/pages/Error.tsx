f79ac9a74ee5ffce96e810e657bbf21efa5eaca4
Ashuksu <ashuksu@gmail.com>
2025-08-12 08:07:34 +0200

    master - fix: replace Link with Button component in Error page for improved styling

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,6 +1,6 @@
-import {APP_PATH} from '~/config/constants';
-import {Link} from 'react-router-dom';
+import {BASE_PATH} from '~/config/constants';
 import PageMeta from '~/components/PageMeta';
+import Button from '~/components/ui/Button';
 import {useTranslation} from 'react-i18next';
 
 export default function Error() {
@@ -14,7 +14,7 @@ export default function Error() {
                 <div className="flex min-h-80 items-center justify-center flex-col text-center">
                     <h1 className="text-4xl font-bold mb-4">{t('errorPage.heading')}</h1>
                     <p className="mb-4">{t('errorPage.text')}</p>
-                    <Link to={APP_PATH} className="hover:underline">{t('errorPage.goHome')}</Link>
+                    <Button href={BASE_PATH}>{t('errorPage.goHome')}</Button>
                     {/* TODO: Create the Error page content */}
                 </div>
             </div>
