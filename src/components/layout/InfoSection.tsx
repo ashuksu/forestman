934255b004bcec0961c4144d4bf96cdf211700ae
Ashuksu <ashuksu@gmail.com>
2025-08-15 22:22:00 +0200

    master - refactored InfoSection to use Image component for icons and updated translation keys for consistency

--- src/components/layout/InfoSection.tsx
+++ src/components/layout/InfoSection.tsx
@@ -2,41 +2,47 @@ import {useTranslation} from 'react-i18next';
 import Container from '~/components/layout/Container';
 import Title from '~/components/ui/Title';
 import InfoCard from '~/components/ui/InfoCard';
-import Icon1 from '@images/icons/1.svg';
-import Icon2 from '@images/icons/2.svg';
-import Icon3 from '@images/icons/3.svg';
+import Image from '~/components/ui/Image';
 
 export default function InfoSection() {
     const {t} = useTranslation();
 
     const infoCardsData = [
         {
-            icon: Icon1,
-            titleKey: 'infoSection.card1Title',
-            textKey: 'infoSection.card1Text',
+            icon: 'icons/1.svg',
+            titleKey: 'infoSection.card1.title',
+            textKey: 'infoSection.card1.text',
         },
         {
-            icon: Icon2,
-            titleKey: 'infoSection.card2Title',
-            textKey: 'infoSection.card2Text',
+            icon: 'icons/2.svg',
+            titleKey: 'infoSection.card2.title',
+            textKey: 'infoSection.card2.text',
         },
         {
-            icon: Icon3,
-            titleKey: 'infoSection.card3Title',
-            textKey: 'infoSection.card3Text',
+            icon: 'icons/3.svg',
+            titleKey: 'infoSection.card3.title',
+            textKey: 'infoSection.card3.text',
         },
     ];
 
     return (
         <section id="info-section" className="py-16 bg-[var(--secondary)]">
             <Container>
-                <Title className="text-center mb-8">{t('infoSection.infoTitle')}</Title>
+                <Title className="text-center mb-8">{t('infoSection.heading')}</Title>
 
                 <div className="flex flex-col items-center md:items-stretch md:flex-row gap-6">
                     {infoCardsData.map((card, index) => (
                         <InfoCard
                             key={index}
-                            icon={<img src={card.icon} alt={`Icon ${index + 1}`} className="w-8 h-8 object-contain"/>}
+                            icon={
+                                <Image
+                                    src={card.icon}
+                                    alt={`Icon ${index + 1}`}
+                                    width={30}
+                                    height={30}
+                                    className="w-8 h-8 object-contain"
+                                />
+                            }
                             title={t(card.titleKey)}
                             text={t(card.textKey)}
                         />
